67c7467fe0be7e4fc87e51973c9f1918628caf00
malkavia <malkavia@163.com>
2015-02-18 20:48:54 +0800

    v0.02 刷新、加载更多

--- iMusic/MainViewController.h
+++ iMusic/MainViewController.h
@@ -9,16 +9,22 @@
 #import <UIKit/UIKit.h>
 #import "ShareAssetLibrary.h"
 #import "UMSocial.h"
-@interface MainViewController : UIViewController <UMSocialUIDelegate>
+@interface MainViewController : UIViewController <UMSocialUIDelegate,UIWebViewDelegate>
 @property (strong,nonatomic)UIWebView *webView;
 @property (strong,nonatomic)NSString *currentURL;
 @property (strong,nonatomic)NSString *currentTitle;
 @property (strong,nonatomic)NSString *currentHTML;
 @property (strong,nonatomic)NSString *weiboUrlAddress;
+@property (strong,nonatomic)NSString *loadingPageAddress;
+@property (strong,nonatomic)NSString *assetUrlString;
+@property (strong, nonatomic)NSURL *coverURL;
 @property (nonatomic)BOOL isChangingWeibo;
-- (IBAction)loadWebView:(id)sender;
+@property (strong,nonatomic) NSString *shareInfo;
+@property (nonatomic)BOOL isInTheFirstPage;
+- (IBAction)shareToWeibo:(id)sender;
 - (IBAction)backToInit:(id)sender;
 - (IBAction)refreshPage:(id)sender;
+- (IBAction)loadMore:(id)sender;
 
 @end
 
--- iMusic/MainViewController.m
+++ iMusic/MainViewController.m
@@ -20,14 +20,14 @@ NSString *defaultUrl = @"http://www.weibo.com/";
     _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 44, 320, 400)];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeWeibo:) name:@"WeiboUrlAddress" object:nil];
     NSString *urlFromInfo = [ShareAssetLibrary getValueForKey:@"WeiboUrlAddress"];
-    
+    self.isInTheFirstPage = YES;
     if (!self.isChangingWeibo) {
         self.weiboUrlAddress = [defaultUrl stringByAppendingString:urlFromInfo];
     }else{
         self.weiboUrlAddress = [defaultUrl stringByAppendingString:self.weiboUrlAddress];
         self.isChangingWeibo = NO;
     }
-
+    
     NSURLRequest *request =[NSURLRequest requestWithURL:[NSURL URLWithString:self.weiboUrlAddress]];
     [_webView loadRequest:request];
     [self.view addSubview:_webView];
@@ -36,19 +36,19 @@ NSString *defaultUrl = @"http://www.weibo.com/";
     [super didReceiveMemoryWarning];
 }
 
--(void) webViewDidFinishLoad:(UIWebView *)webView {
-    
-    [UIApplication sharedApplication].networkActivityIndicatorVisible =NO;
-    self.title =  [webView stringByEvaluatingJavaScriptFromString:@"document.title"];//获取当前页面的title
-    self.currentURL = webView.request.URL.absoluteString;
-    NSString *lJs = @"document.documentElement.innerHTML";//获取当前网页的html
-    self.currentHTML = [webView stringByEvaluatingJavaScriptFromString:lJs];
-    NSString *shareInfo =  [self getUsefulString:self.currentHTML];
-    [self shareToWeChat:shareInfo];
+
+- (void)shareToWeibo:(id)sender{
+    self.shareInfo = [self getUsefulString:_webView];
+    [self setWechatInfo:self.shareInfo];
+
 }
-- (void)loadWebView:(id)sender{
-    [self webViewDidFinishLoad:_webView];
+
+-(void)scrollToTheEnd{
+    NSInteger height = [[_webView stringByEvaluatingJavaScriptFromString:@"document.body.offsetHeight;"] intValue];
+    NSString* javascript = [NSString stringWithFormat:@"window.scrollBy(0, %ld);", height];
+    [_webView stringByEvaluatingJavaScriptFromString:javascript];
 }
+
 
 - (IBAction)backToInit:(id)sender {
      UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
@@ -57,12 +57,48 @@ NSString *defaultUrl = @"http://www.weibo.com/";
 }
 
 - (IBAction)refreshPage:(id)sender {
-    NSURLRequest *request =[NSURLRequest requestWithURL:[NSURL URLWithString:self.weiboUrlAddress]];
-    [_webView loadRequest:request];
+    NSURLRequest *request ;
+    if (self.loadingPageAddress==nil) {
+        self.loadingPageAddress = @"";
+    }
+    if ([self.loadingPageAddress isEqualToString:@""]) {
+        request =[NSURLRequest requestWithURL:[NSURL URLWithString:self.weiboUrlAddress]];
+    }else{
+        request =[NSURLRequest requestWithURL:[NSURL URLWithString:self.loadingPageAddress]];
+    }
+        [_webView loadRequest:request];
 }
 
-- (NSString *)getUsefulString: (NSString *)html{
+- (IBAction)loadMore:(id)sender {
+    if (self.isInTheFirstPage) {
+        [self getUsefulString:_webView];
+        NSURLRequest *request =[NSURLRequest requestWithURL:[NSURL URLWithString:self.loadingPageAddress]];
+        [_webView loadRequest:request];
+        self.isInTheFirstPage = NO;
+    }else{
+        [self scrollToTheEnd];
+    }
+}
+
+- (NSString *)getUsefulString: (UIWebView *)webView{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible =NO;
+    self.title =  [webView stringByEvaluatingJavaScriptFromString:@"document.title"];//获取当前页面的title
+    self.currentURL = webView.request.URL.absoluteString;
+    NSString *lJs = @"document.documentElement.innerHTML";//获取当前网页的html
+    self.currentHTML = [webView stringByEvaluatingJavaScriptFromString:lJs];
+    NSString *html = self.currentHTML;
     if ([html containsString:@"default-content txt-xl"]){
+   //     NSLog(@"%@",html);
+        
+        NSRange allWeiboRangeEnd = [html rangeOfString:@"_-_WEIBO_SECOND_PROFILE_WEIBO"];
+        NSString *allWeiboAddressString = [html substringToIndex:allWeiboRangeEnd.location];
+        NSRange allWeiboRangeStart = [html rangeOfString:@"containerid="];
+        allWeiboAddressString = [allWeiboAddressString substringFromIndex:allWeiboRangeStart.location];
+        allWeiboAddressString = [[NSString stringWithFormat:@"http://m.weibo.cn/page/tpl?"] stringByAppendingString:allWeiboAddressString];
+       allWeiboAddressString = [allWeiboAddressString stringByAppendingString:@"_-_WEIBO_SECOND_PROFILE_WEIBO"];
+        NSLog(@"%@",allWeiboAddressString);
+        _loadingPageAddress = allWeiboAddressString;
+        
         NSRange range = [html rangeOfString:@"default-content txt-xl"];
         NSString *subString = [html substringFromIndex:range.location+24];
        // subString = [subString substringToIndex:500];
@@ -70,7 +106,7 @@ NSString *defaultUrl = @"http://www.weibo.com/";
         NSRange urlRange = [subString rangeOfString:@"<a data-url"];
         NSString *urlString = [subString substringFromIndex:urlRange.location+13];
         urlString = [urlString substringToIndex:19];
-        _weiboUrlAddress = urlString;
+        _assetUrlString = urlString;
         NSRange singgerFrom = [subString rangeOfString:@"cDesc"];
         NSString *singgerString = [subString substringFromIndex:singgerFrom.location+7];
         NSRange singgerTo = [singgerString rangeOfString:@"</"];
@@ -83,7 +119,7 @@ NSString *defaultUrl = @"http://www.weibo.com/";
     
     
         NSString *finalString = [NSString stringWithFormat:@"分享了 %@ 的歌曲《%@》，点击试听",singgerString,titleString];
-        NSLog(@"%@",finalString);
+//        NSLog(@"%@",finalString);
         
         NSRange coverRangeFrom = [subString rangeOfString:@"media-main"];
         NSString *coverString = [subString substringFromIndex:coverRangeFrom.location];
@@ -91,28 +127,28 @@ NSString *defaultUrl = @"http://www.weibo.com/";
         coverString = [coverString substringFromIndex:coverRangeFrom2.location+2];
         NSRange coverRangeTo = [coverString rangeOfString:@"height"];
         coverString = [coverString substringToIndex:coverRangeTo.location-2];
-        
-        NSLog(@"%@",coverString);
-
+        NSLog(@"coverString : %@",coverString);
+        self.coverURL = [NSURL URLWithString:coverString];
         return finalString;
     }else{
         return @"";
     }
 }
 
--(void)shareToWeChat:(NSString *)shareInfo{
-    
+-(void)setWechatInfo:(NSString *)shareInfo{
+    NSLog(@"coverString : %@",self.coverURL);
+
     [UMSocialSnsService presentSnsIconSheetView:self
                                          appKey:@"545acb60fd98c5e074008da5"
                                       shareText:shareInfo
-                                     shareImage:[UIImage imageNamed:@"icon.png"]
+                                     shareImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:self.coverURL]]
                                 shareToSnsNames:[NSArray arrayWithObjects:UMShareToSina,UMShareToSms,UMShareToWechatSession,UMShareToWechatTimeline,nil]
                                        delegate:self];
-    NSLog(@"%@",_weiboUrlAddress);
-    [UMSocialData defaultData].extConfig.wechatSessionData.url =_weiboUrlAddress;
-    NSRange range = [_weiboUrlAddress rangeOfString:@"//"];
-    NSString *timelineUrl = [_weiboUrlAddress substringFromIndex:range.location+2];
-    NSLog(@"timelineUrl + %@",timelineUrl);
+//    NSLog(@"%@",_assetUrlString);
+    [UMSocialData defaultData].extConfig.wechatSessionData.url =_assetUrlString;
+    NSRange range = [_assetUrlString rangeOfString:@"//"];
+    NSString *timelineUrl = [_assetUrlString substringFromIndex:range.location+2];
+//  NSLog(@"timelineUrl + %@",timelineUrl);
     [UMSocialData defaultData].extConfig.wechatTimelineData.url = timelineUrl;
 }
 
@@ -122,6 +158,7 @@ NSString *defaultUrl = @"http://www.weibo.com/";
     NSString *url = sender.description;
     self.weiboUrlAddress = url;
 }
+
 
 @end
 
